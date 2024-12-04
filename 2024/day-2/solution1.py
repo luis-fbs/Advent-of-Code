97217ba332e6dd40b12cfe226ba6f3bfa1189c92
luis-fbs <luis-fbs@outlook.com>
2024-12-04 15:16:47 -0300

    Solution 2.1

---
+++ 2024/day-2/solution1.py
@@ -0,0 +1,25 @@
+def report_is_safe(report):
+	growth = report[0] - report[1]
+
+	for x in range(1, len(report)):
+		diff = report[x-1] - report[x]
+
+		if diff * growth < 0:
+			return False
+
+		if diff < 0:
+			diff *= -1
+
+		if not 1 <= diff <= 3:
+			return False
+	return True
+
+safe_reports = 0
+
+with open("input", "r") as file:
+	for reports in file:
+		report = [ int(x) for x in reports.split() ]
+		if report_is_safe(report):
+			safe_reports += 1
+
+print(safe_reports)
